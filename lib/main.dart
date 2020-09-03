61f5fafc7b0c39230704ec735a86379eb6fd8d23
Gaius G D'cruz <gaingaius123@gmail.com>
2020-09-04 05:10:35 +0530

    route updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trailapp/Screens/Showall/showalldetails.dart';
 import 'Screens/Homepage/homepage.dart';
 
 void main() {
@@ -15,6 +16,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      routes: {
+        '/showalldetails': (context)=> ShowAllDetails()
+      },
       home: Homepage(),
     );
   }
