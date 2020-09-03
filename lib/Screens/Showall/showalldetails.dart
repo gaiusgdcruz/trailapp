e3f4b07bcae4c265c6c3d5e9e7745b83b7c7b5cd
Gaius G D'cruz <gaingaius123@gmail.com>
2020-09-04 05:11:46 +0530

    update

---
+++ lib/Screens/Showall/showalldetails.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ShowAllDetails extends StatelessWidget {
+  static const routeName = "/showalldetails";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+    children: [
+      Stack(
+         children: [
+           Image.asset('assets/img/home1.jpg'),
+           Align(
+             alignment: Alignment.topLeft,
+             child: Column(
+               mainAxisSize: MainAxisSize.min,
+               children: [
+                 SizedBox(height: 30),
+                 IconButton(icon: Icon(Icons.arrow_back_ios),
+                 onPressed: () {Navigator.of(context).pop();})
+               ]
+           )
+           )
+         ]
+      )
+    ],
+      ),
+    );
+  }
+}
